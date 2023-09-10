3be88110e052c3222658ec4c13c686901a736b3a
Ihor Bobulych <ihbobuacc@gmail.com>
2023-09-10 17:47:24 +0300

    Change server

--- src/features/service/ShipmentsService.ts
+++ src/features/service/ShipmentsService.ts
@@ -3,6 +3,6 @@ import {IShipment} from "../../shared/models";
 import {$api} from "../../shared/http";
 export class ShipmentsService {
     static async fetchShipments(): Promise<AxiosResponse<IShipment[]>> {
-        return $api.get<IShipment[]>('orders');
+        return $api.get<IShipment[]>('shipments.json?key=5e0b62d0');
     }
 }
--- src/shared/http/axios.ts
+++ src/shared/http/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = 'http://localhost:8080/';
+const API_URL = 'https://my.api.mockaroo.com/';
 
 export const $api = axios.create({
     baseURL: API_URL,
